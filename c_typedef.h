96987f306350aa54ce555b05f28e6d4d2513550d
root <root@G1-BG1043.i-jimi.com.cn>
2024-12-27 18:19:40 +0800

    add c file

--- c_typedef.h
+++ c_typedef.h
@@ -1,13 +1,11 @@
 /*
  * @Author: liz kishimeru474@gmail.com
  * @Description: 
- * @Date: 2024-12-27 18:00:00
+ * @Date: 2024-12-27 18:14:40
  * Copyright (c) liz, All Rights Reserved. 
  */
-#ifndef __C_VECTOR_H__
-#define __C_VECTOR_H__
+#ifndef __C_TYPEDEF_H__
+#define __C_TYPEDEF_H__
 
 
-
-
-#endif // __C_VECTOR_H__
+#endif // __C_TYPEDEF_H__
---
+++ c_vector.c
@@ -0,0 +1,11 @@
+/*
+ * @Author: liz kishimeru474@gmail.com
+ * @Description: 
+ * @Date: 2024-12-27 18:17:39
+ * Copyright (c) liz, All Rights Reserved. 
+ */
+#include "c_vector/c_vector.h"
+#include "c_vector/c_typedef.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
